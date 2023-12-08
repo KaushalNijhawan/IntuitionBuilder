e502242eaa64b5f43438c70f6c0719ae7ab9df0c
KaushalNijhawan <kaushalnijhawan3641@gmail.com>
2023-12-08 12:43:21 +0000

    creating intial Auth files

---
+++ components/Authentication/Login/Login.tsx
@@ -0,0 +1,11 @@
+import { View , Text} from "react-native";
+
+const Login = () =>{
+    return(
+        <View>
+            <Text> this is loginPage!</Text>
+        </View>
+    );
+}
+
+export default Login;
